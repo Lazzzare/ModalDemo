eb510902391aeb9f84f386fd18b5eb2e777bbcba
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-06 23:57:19 +0400

    Update App.tsx

--- modal/src/App.tsx
+++ modal/src/App.tsx
@@ -1,9 +1,27 @@
+import Modal from "./components/Modal";
+import { useState } from "react";
+
 function App() {
+  const [isopen, setIsOpen] = useState(false);
+
+  interface props {
+    open: String;
+  }
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setIsOpen(!isopen);
+    console.log(isopen);
+  };
+
   return (
-    <div className="h-screen w-full bg-blue-400 mx-auto flex justify-center items-center text-center">
-      <button className="bg-black text-emerald-900 border border-white px-3 py-1 rounded">
+    <div className="h-screen w-full bg-blue-800 mx-auto flex justify-center items-center text-center">
+      <button
+        onClick={handleClick}
+        className="bg-black text-red-300 border border-black px-3 py-1 rounded-md"
+      >
         დააჭირე მარიამ...
       </button>
+      <Modal open={isopen} onClose={() => setIsOpen(false)} />
     </div>
   );
 }
