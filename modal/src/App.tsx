9a86dddc5f23fc9afce9fb164156581d6dbd64e1
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-07 18:38:53 +0400

    add HandleClick

--- modal/src/App.tsx
+++ modal/src/App.tsx
@@ -4,10 +4,16 @@ import Modal from "./components/Modal";
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
+  const handleClick = () => {
+    alert("hell");
+  };
 
   return (
     <div>
-      <button onClick={() => setOpenModal(true)} className="modalButton">
+      <button
+        onClick={() => setOpenModal(true)}
+        className="modalButton border border-black rounded text-black"
+      >
         დააჭირე მარიამ...
       </button>
       <Modal open={openModal} onClose={() => setOpenModal(false)} />
