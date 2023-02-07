e84838dc4295c0ef7377b9d7b28c7529d1aae7c9
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-07 18:38:41 +0400

    Update Modal.tsx

--- modal/src/components/Modal.tsx
+++ modal/src/components/Modal.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import Image from "../assets/img.jpg";
+import Swal from "sweetalert2";
 
 const Modal = ({ open, onClose }) => {
   if (!open) return null;
+
+  const handleClick = () => {
+    Swal.fire({
+      title: "Sweet!",
+      text: "Modal with a custom image.",
+      imageUrl: "https://unsplash.it/400/200",
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: "Custom image",
+    });
+  };
   return (
     <div onClick={onClose} className="overlay">
       <div
@@ -13,21 +25,31 @@ const Modal = ({ open, onClose }) => {
       >
         <img src={Image} alt="/" />
         <div className="modalRight">
-          <p className="closeBtn" onClick={onClose}>
+          <p className="closeBtn cursor-pointer" onClick={onClose}>
             X
           </p>
           <div className="content">
-            <p>Do you want a</p>
-            <h1>$20 CREDIT</h1>
-            <p>for your first tade?</p>
+            <p className="">და დააააამ!!!!</p>
+            <h1>გილოცავ მარიამ, დღეს შენ</h1>
+            <p>ორი მუსიკით დაჯილდოვდი</p>
           </div>
-          <div className="btnContainer">
-            <button className="btnPrimary">
-              <span className="bold">YES</span>, I love NFT's
-            </button>
-            <button className="btnOutline">
-              <span className="bold">NO</span>, thanks
-            </button>
+          <div className="flex justify-center gap-4">
+            <a
+              target={"_blank"}
+              href="https://www.youtube.com/watch?v=zbp6mNzy9bg"
+            >
+              <button className="bg-blue-500 border-blue-500 rounded-md px-4 py-2">
+                <span className="bold">1</span>) სიარული
+              </button>
+            </a>
+            <a
+              target={"_blank"}
+              href="https://www.youtube.com/watch?v=NFxYVVCd96w"
+            >
+              <button className="bg-blue-500 border-blue-500 rounded-md px-4 py-2">
+                <span className="bold">2</span>) any1
+              </button>
+            </a>
           </div>
         </div>
       </div>
